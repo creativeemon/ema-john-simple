52bb47eb108a5deb25134015f58ccbdb665bb908
MD:sahariar Ahammed Emon <sahariargd28@gmail.com>
2023-03-28 14:58:01 +0600

    show data from car

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,10 +8,16 @@ const Cart = ({cart}) => {
 
     let priceTotal = 0;
     let totalShipping = 0;
-
+    let quantity = 0;
     for(const product of cart){
-        priceTotal = priceTotal + product.price;
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+        // shot cat
+        // product.quantity = product.quantity || 1;
+        priceTotal = priceTotal + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = priceTotal*7/100;
     const grandTotal = priceTotal + totalShipping + tax;
@@ -19,7 +25,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h4>Oder Summary</h4>
-            <p>Selected Items:{cart.length}</p>
+            <p>Selected Items:{quantity}</p>
             <p>Total Price:${priceTotal}</p>
             <p>Total Shipping:${totalShipping}</p>
             <p>tax:{tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -39,10 +39,24 @@ const Shop = () => {
         setCart(savedCart)
     }, [products])
     const handlerAddCart = (product) => {
-        const newCart = [...cart, product];
-        setCart(newCart)
-        addToDb(product.id)
-    }
+        let newCart = [];
+        // const newCart = [...cart, product];
+        // if product doesn't exist in tje cart, then set quantity=1
+        // if exist update quantity by the 1
+        const exists = cart.find(pd => pd.id === product.id)
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product);
+                newCart = [...remaining , exists]
+            }
+
+            setCart(newCart)
+            addToDb(product.id)
+        }
 
     return (
         <div className='shop-container'>
